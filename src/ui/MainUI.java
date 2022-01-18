e0a928c4a60bec75f196fab5280ba288d424c67e
fihan <fihan@localhost.localdomain>
2022-01-18 21:20:40 +0900

    menambahkan filed id di panel input absensi dan tabel data absensi

--- src/ui/MainUI.java
+++ src/ui/MainUI.java
@@ -21,7 +21,7 @@ public class MainUI extends JFrame{
   private JLabel pertemuanLabel;
   private JPanel listAbsensiPanel;
   private JPanel inputAbsensiPanel;
-  private JTable showTable;
+  private JTable tableDataAbsensi;
   private JButton buttonEdit;
   private JButton buttonHapus;
   private JButton resetButton;
@@ -30,6 +30,7 @@ public class MainUI extends JFrame{
   private JButton button1;
   private JButton button2;
   private JButton button4;
+  private JTextField idField;
 
   // constructor
   public MainUI() {
@@ -52,20 +53,23 @@ public class MainUI extends JFrame{
       }
     });
 
-    showTable.addMouseListener(new MouseAdapter() {
+    tableDataAbsensi.addMouseListener(new MouseAdapter() {
       @Override
       public void mouseClicked(MouseEvent e) {
         super.mouseClicked(e);
         System.out.println("berhasil klik tabel");
-        int row = showTable.getSelectedRow();
-        String noData = showTable.getModel().getValueAt(row, 0).toString();
+        int row = tableDataAbsensi.getSelectedRow();
+        String noData = tableDataAbsensi.getModel().getValueAt(row, 0).toString();
         System.out.println(noData);
       }
     });
   }
 
   private void regenerateAllCombobox() {
-    // comboboxNama
+    // id field
+    idField.setText("");
+
+    // combobox Nama
     namaField.removeAllItems();
     String itemNama[] = {"-pilih-", "aco", "budi", "joni"};
     namaField.setModel(new DefaultComboBoxModel(itemNama));
@@ -88,16 +92,16 @@ public class MainUI extends JFrame{
 
   private void createTable() {
     Object[][] data = {
-            {1, "joni", "fisika", 1, "hadir"},
-            {2, "budi", "PBO", 1, "alpha"},
-            {3, "aco", "web programming", 2, "sakit"}
+            {123, "joni", "fisika", 1, "hadir"},
+            {234, "budi", "PBO", 1, "alpha"},
+            {345, "aco", "web programming", 2, "sakit"}
     };
     // set table
-    showTable.setModel(new DefaultTableModel(
+    tableDataAbsensi.setModel(new DefaultTableModel(
             data,
-            new String[] {"no", "nama", "mata kuliah", "pertemuan" ,"keterangan"} // column
+            new String[] {"ID", "nama", "mata kuliah", "pertemuan" ,"keterangan"} // nama column
     ));
-    TableColumnModel columns =  showTable.getColumnModel();
+    TableColumnModel columns =  tableDataAbsensi.getColumnModel();
     columns.getColumn(0).setMinWidth(15);
     columns.getColumn(3).setMinWidth(10);
 
